5eeb026efa79eebce0f44e2a3a781ad85a854ea0
Joseph Stahl <hello@josephstahl.com>
2018-10-07 15:56:22 -0500

    update some webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,43 @@
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  entry: './src/styles.css',
+  entry: "./src/styles.css",
   mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader',
-          ],
-        }),
-      },
-    ],
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          "postcss-loader"
+        ]
+      }
+    ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.css', {
-      disable: process.env.NODE_ENV === 'development',
+    // new ExtractTextPlugin('styles.css', {
+    //   disable: process.env.NODE_ENV === 'developnment',
+    // }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
     }),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-  ],
-}
+      filename: "index.html",
+      template: "src/index.html"
+    })
+  ]
+};
